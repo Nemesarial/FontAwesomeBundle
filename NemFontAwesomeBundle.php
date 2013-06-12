4b0a8807399fe94ad60a8310c512a6abb55851f4
Mark Holtzhausen <nemesarial@gmail.com>
2013-06-12 12:24:39 +0200

    Changed the Bundle Name

---
+++ NemFontAwesomeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Nemesarial\FontAwesomeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NemFontAwesomeBundle extends Bundle
+{
+}
--- QlFontAwesomeBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ql\FontAwesomeBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class QlFontAwesomeBundle extends Bundle
-{
-}
